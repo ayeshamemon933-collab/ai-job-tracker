306150dbb90bb3e66758fb384adecfbe5a5cc68a
Ayesha <ayeshamemon933@gmail.com>
2026-03-30 22:13:00 +0530

    updated API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://ai-job-tracker-8tk7.onrender.com";
 
 export const getJobs = async () => {
   const res = await fetch(`${BASE_URL}/jobs`);
