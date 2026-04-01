973c82f50fa73c63448d2a138e8284fe15fe0be1
Ayesha <ayeshamemon933@gmail.com>
2026-04-01 13:46:37 +0530

    Update api.js to use REACT_APP_API_URL env variable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,38 +1,36 @@
-const BASE_URL = "https://ai-job-tracker-8tk7.onrender.com";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
+// GET ALL JOBS
 export const getJobs = async () => {
   const res = await fetch(`${BASE_URL}/jobs`);
   return res.json();
 };
 
+// ADD JOB
 export const addJob = async (job) => {
   const res = await fetch(`${BASE_URL}/jobs/add`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(job),
   });
   return res.json();
 };
 
+// AI DESCRIPTION
 export const getAISuggestion = async (data) => {
   const res = await fetch(`${BASE_URL}/ai/suggest`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   });
   return res.json();
 };
 
+// AI MATCH
 export const getAIMatch = async (data) => {
   const res = await fetch(`${BASE_URL}/ai/match`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   });
   return res.json();
